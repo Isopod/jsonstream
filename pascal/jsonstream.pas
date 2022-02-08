409c4ae6f67dac4d96aa46628adabaa701b59b3c
Philip Zander <philip.zander@gmail.com>
2022-02-08 11:43:47 +0100

    Release version 1.0.2

--- pascal/jsonstream.pas
+++ pascal/jsonstream.pas
@@ -20,6 +20,8 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 // Changelog:
+//   2022-02-08: Release version 1.0.2
+//               - Fixes a rarely triggered off-by-one error
 //   2021-12-23: Release version 1.0.1
 //               - Fixes assertion failure on skipping boolean value
 //   2021-10-18: Release version 1.0.0
